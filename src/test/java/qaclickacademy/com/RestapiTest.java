ec38ca623d9b2f9c23f92e3bfd16000785615b5e
kranthihere116 <kranthikumarhere@gmail.com>
2021-03-16 01:33:20 +0530

    us guy commit

--- src/test/java/qaclickacademy/com/RestapiTest.java
+++ src/test/java/qaclickacademy/com/RestapiTest.java
@@ -9,6 +9,9 @@ public class RestapiTest {
 	System.out.println("RestApiTest1");
 	System.out.println("RestApiTest2");	
 	System.out.println("RestApiTest3");	
+	System.out.println("RestApiTest4");	
+	System.out.println("RestApiTest5");	
+	System.out.println("RestApiTest6");	
 	}
 	@Test
 	public void RestApiTest2()
